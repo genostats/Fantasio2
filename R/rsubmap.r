159cdac94eb9624a7bdc998b5502f7e4c97f8a9e
Margot Derouin <margot.derouin@inserm.fr>
2022-05-31 11:39:48 +0200

    create random submap by hotspots

---
+++ R/rsubmap.r
@@ -0,0 +1,19 @@
+rsubmap <- function(segmentsList){
+  
+  segmentSummary <- segments.list.summary(segmentsList)
+  shift <- cumsum(segmentSummary$number_of_segments)
+  shift <- append(0, shift)
+  max <- shift[length(shift)]
+  
+  submap <- numeric(max)
+  
+  for(chr in seq_along(segmentsList))
+  {
+    chrMarker <- segmentsList[[chr]]
+    randomMarkerVector <- get.marker.chromosome(chrMarker)
+    submap[(shift[chr]+1):shift[chr+1]] <- randomMarkerVector
+  }
+  
+  submap
+}
+
