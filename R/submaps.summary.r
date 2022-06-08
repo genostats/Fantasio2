f2d83d5f74baa61d5d460fbb4ccf85c91de03402
Margot Derouin <margot.derouin@inserm.fr>
2022-06-08 10:10:21 +0200

    update with p.lrt

--- R/submaps.summary.r
+++ R/submaps.summary.r
@@ -29,15 +29,15 @@ submaps.summary <- function (bedmatrix, a, f, a.threshold = 1) {
   w.a <- (a > a.threshold)
   f[w.a] <- NA
   a[w.a] <- NA
-  #p.lrt[w.a] <- NA
-  #l <- (p.lrt < 0.05)
-  #pLRT_median <- apply(p, 1, median, na.rm=TRUE)
+  p.lrt[w.a] <- NA
+  l <- (p.lrt < 0.05)
+  pLRT_median <- apply(p, 1, median, na.rm=TRUE)
   
-  #nValidSubmap <- numeric(nrow(l))
-  #for (i in seq_len(nrow(l)))
-  #{
-  #  nValidSubmap[i] <- sum(l[i,], na.rm = TRUE)
-  #}
+  nValidSubmap <- numeric(nrow(l))
+  for (i in seq_len(nrow(l)))
+  {
+    nValidSubmap[i] <- sum(l[i,], na.rm = TRUE)
+  }
   
   submaps_used <- rowSums(sapply(as.data.frame(a), function(x) x <= a.threshold), na.rm = TRUE )
   quality <- (submaps_used*100)/dim(f)[2]
@@ -48,15 +48,14 @@ submaps.summary <- function (bedmatrix, a, f, a.threshold = 1) {
                    submaps       = submaps_used,
                    quality       = quality,
                    f_median      = apply(f, 1, median, na.rm=TRUE),
-                   a_median      = apply(a, 1, median, na.rm=TRUE)#,
-                   #pLRT_median   = pLRT_median,
-                   #inbred        = pLRT_median < 0.05,
-                   #pLRT_inf_0.05 = nValidSubmap
+                   a_median      = apply(a, 1, median, na.rm=TRUE),
+                   pLRT_median   = pLRT_median,
+                   inbred        = pLRT_median < 0.05,
+                   pLRT_inf_0.05 = nValidSubmap
   )
   for(i in seq_len(nrow(df))) {
     if(!is.finite(df$f_median[i]))
-      #df[i,5:10] <- NA
-      df[i,5:7] <- NA
+      df[i,5:10] <- NA
   }
   
   return(df)
