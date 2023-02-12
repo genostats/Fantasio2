e89740bc495bb3b62c93dbc5e230fb16e95731ed
SidonieFoulon <sidonie.foulon@inserm.fr>
2023-02-12 19:22:35 +0100

    recap.HBD.FLOD function

--- R/recap.HBD.FLOD.r
+++ R/recap.HBD.FLOD.r
@@ -1,5 +1,5 @@
 #' @export
-recap.HBD.FLOD <- function(atlas, keep.inds, q, recap) {
+recap.HBD.FLOD <- function(atlas, keep.inds, q, recap, median=FALSE) {
   if(recap != "SNP") stop("Not yet implemented")
 
   # shotcuts for atlas slots
@@ -16,9 +16,12 @@ recap.HBD.FLOD <- function(atlas, keep.inds, q, recap) {
   wi <- which(keep.inds)
   
 
-  x <- list() #créer liste avec 2 grandes matrices phbd et FLOD
+  x <- list() #créer liste pour les 2 grandes matrices phbd et FLOD
 
-  
+  if(median==TRUE) {
+    a <- summary$a_median
+    f <- summary$f_median
+    }
   
   #première boucle pour créer la grande sous-carte = union des snps tirés dans les n sous-cartes
   big.submap <- as.integer(vector())
@@ -27,10 +30,10 @@ recap.HBD.FLOD <- function(atlas, keep.inds, q, recap) {
     submap <- rsubmap(segments.list)
     big.submap <- union(big.submap,submap)
   }
-  big.submap <- sort(big.submap) #ON GARDE ?
+  big.submap <- sort(big.submap) 
   
-  big.HBD <- matrix(0, nrow = length(wi), ncol = length(big.submap)) #matrice des phbd avec 1 ligne par individu consanguin et 1 colonne par snp tiré
-  big.FLOD <- matrix(0, nrow = length(wi), ncol = length(big.submap)) #matrice des flod avec 1 ligne par individu consanguin et 1 colonne par snp tiré
+  big.HBD <- matrix(0, ncol = length(wi), nrow = length(big.submap)) #matrice des phbd avec 1 colonne par individu consanguin et 1 ligne par snp tiré
+  big.FLOD <- matrix(0, ncol = length(wi), nrow = length(big.submap)) #matrice des flod avec 1 colonne par individu consanguin et 1 ligne par snp tiré
   
   
   for(i in 1:n) { # boucle sur les cartes
@@ -40,40 +43,50 @@ recap.HBD.FLOD <- function(atlas, keep.inds, q, recap) {
     d.dist <- delta.dist(bedmatrix, big.submap)
 
     # les a et f pour les individus qui nous intéressent, pour la carte en cours
-    a <- A[wi, i]
-    f <- F[wi, i]
+    #a <- A[wi, i]
+    #f <- F[wi, i]
+    
+    # les a et f pour la carte en cours
+    if(median==FALSE){
+      a <- A[, i]
+      f <- F[, i]
+    }
+    
 
     # matrice des pHBD [une colonne par individu, une ligne par SNP]
     
     #créer vecteur freq.submap de NA de longueur big.submap puis remplacer par les freq aux positions tirées
     freq.submap <- rep(NA, times = length(bedmatrix@p))
     freq.submap[submap] <- bedmatrix@p[submap]
-    HBD <- probaHBD(bedmatrix@bed, freq.submap, big.submap, d.dist, keep.inds, a = A[,i], f = F[,i], epsilon) #renvoie les snps sur les lignes et les inds sur les colonnes
+    HBD <- probaHBD(bedmatrix@bed, freq.submap, big.submap, d.dist, keep.inds, a = a, f = f, epsilon) #renvoie les snps sur les lignes et les inds sur les colonnes
     HBD[!is.finite(HBD)] <- 0
-    HBD <- t(HBD) # pour avoir les individus sur lignes et snps sur les colonnes comme dans big.submap
-
+   
+    # extraction du f pour les individus conservés
+    ff <- f[wi]
     # matrice des FLOD (une colonne par individu)
     FLOD <- log10(HBD + q * (1 - HBD))
     # chaque colonne doit etre divisée par ( f + q * (1 - f) ) [ soustraction à l'échelle log10 ]
     # (on pourrait utiliser sweep mais niveau gestion mémoire ceci doit être plus efficace)
     for(j in 1:ncol(FLOD))
-      FLOD[,j] <- FLOD[,j] - log10( f[j] + q * (1 - f[j]) )
+      FLOD[,j] <- FLOD[,j] - log10( ff[j] + q * (1 - ff[j]) )
 
+    
     big.HBD <- big.HBD + HBD 
     big.FLOD <- big.FLOD + FLOD  
   }
   setRandomSeed(old.seed) # restoring seed
 
+  
   # calcule les matrices moyennes des HBD / FLOD snp par snp
-  # ces matrices ont une ligne par individu / une colonne par SNP
-  x$HBD <- big.HBD/n #remplacé par division
-  x$FLOD <- big.FLOD/n #remplacé par division
+  # ces matrices doivent avoir une ligne par individu / une colonne par SNP
+  x$HBD <- t(big.HBD)/n #on transpose pour la suite
+  x$FLOD <- t(big.FLOD)/n #on transpose pour la suite
   rownames(x$HBD) <- rownames(x$FLOD) <- uniqueIds(summary$famid[wi], summary$id[wi])
   colnames(x$HBD) <- colnames(x$FLOD) <- bedmatrix@snps$id[big.submap] #snps vus dans grande sous carte
 
   # c'est fini.
-  atlas@HBD_recap <- x$HBD     #!
-  atlas@FLOD_recap <- x$FLOD   #!
+  atlas@HBD_recap <- x$HBD     
+  atlas@FLOD_recap <- x$FLOD   
   atlas@recap <- recap
   atlas@q <- q
   atlas
