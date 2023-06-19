7fcfb1c6114f841f21ff7f2a08f2bcf43054e599
HervePerdry <herve.perdry@gmail.com>
2023-06-19 14:06:01 +0200

    corrected bug in recap.HBD.FLOD.sparse

--- R/recap.HBD.FLOD.sparse.r
+++ R/recap.HBD.FLOD.sparse.r
@@ -37,14 +37,17 @@ recap.HBD.FLOD.sparse <- function(atlas, keep.inds, q, recap, median) {
     HBD <- probaHBD(bedmatrix@bed, bedmatrix@p, submap, d.dist, keep.inds, a = a, f = f, epsilon)
     HBD[!is.finite(HBD)] <- 0
 
+    # les f et a pour les individus de keep inds
+    ff <- f[wi]
+    aa <- a[wi]
     # matrice des FLOD (une colonne par individu)
     FLOD <- log10(HBD + q * (1 - HBD))
     # chaque colonne doit etre divisée par ( f + q * (1 - f) ) [ soustraction à l'échelle log10 ]
     # (on pourrait utiliser sweep mais niveau gestion mémoire ceci doit être plus efficace)
     for(j in 1:ncol(FLOD))
-      FLOD[,j] <- FLOD[,j] - log10( f[j] + q * (1 - f[j]) )
+      FLOD[,j] <- FLOD[,j] - log10( ff[j] + q * (1 - ff[j]) )
 
-    h <- updateHashProbas(h, submap, (a < 1), HBD, FLOD)
+    h <- updateHashProbas(h, submap, (aa < 1), HBD, FLOD)
   }
   setRandomSeed(old.seed) # restoring seed
 
