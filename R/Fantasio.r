7e4c2f22ab0d38d2d65776240bce688ff9098637
Margot Derouin <margot.derouin@inserm.fr>
2023-06-21 11:46:02 +0200

    add documentation

--- R/Fantasio.r
+++ R/Fantasio.r
@@ -1,6 +1,38 @@
+#' Wrapper for the package Fantasio
+#' 
+#' This function is used as a wrapper for the package Fantasio to create segments list and submaps
+#' 
+#' @param bedmatrix a bed.matrix 
+#' @param segment.options a list of arguments to the function that will create the segments list
+#' @param n the number of submaps (default is 100)
+#' @param min.quality minimal quality (in \%) to include an inbred individual into the analysis (default is 95)
+#' @param list.id a list of individuals of interest ('famid:id') (default = no list id)
+#' @param recap if you want the summary of probabilities by snps or by segments (only by SNPs for the moment)
+#' @param phen.code phenotype coding :
+#'        - 'R' : 0:control ; 1:case ; NA:unknown 
+#'        - 'plink' : 1:control ; 2:case ; 0/-9/NA:unknown (default)
+#' @param q assumed frequency of the mutation involved in the disease for each individual (default is 0.0001)
+#' @param epsilon genotype error rate (default is 0.001)
+#' @param median define the f and a parameters used to compute pHBD and FLOD
+	   - if FALSE : f and a estimated on each submap
+	   - if TRUE : median value of estimations on all submaps of f and a (default) 
+#' @param dense.recap define how pHBD and FLOD are combined over the submaps
+	   - if FALSE : pHBD and FLOD are combined SNP by SNP with a mean on the number of submaps that includes this SNP
+	   - if TRUE : mean of pHBD and FLOD on all submaps (default)
+
+
+
+#' @details This function is a wrapper to make the usage of the package easier. The function calls different functions: 
+#' @details The first function, `segments.list.by.hotspots` is used to create a list of segments. 
+#' @details The second function, `atlas` is used to create submaps based on recombination hotspots (for the moment).
+#' @details The arguments that can be included in `segment.options` are described in `segments.list.by.hotspots`.
+#' @details If `recap = 'SNP'`, the quantities such as HBD probabilities, FLOD, HFLOD, 
+#'   are recapitulated SNP by SNP (default).
+
+
 #' @export Fantasio
 
-#test sidonie
+
 # pour l'instant, que "by hotspots" avec un summary "by SNPs"
 Fantasio <- function(bedmatrix, segment.options, n = 100, min.quality = 95, list.id, 
                      recap = c("SNP", "segment"), phen.code = c("plink", "R"), q = 1e-4, epsilon = 1e-3, median = TRUE, dense.recap = TRUE) {
