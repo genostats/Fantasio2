4b801f094bc35aa0daa32c7a93bd9c80f3613356
HervePerdry <herve.perdry@gmail.com>
2023-09-11 17:51:13 +0200

    rm R function replaced by C++ function

--- R/random.snp.r
+++
@@ -1,9 +0,0 @@
-random.snp <- function(x) {
-  beg <- x$beg
-  end <- x$end
-  submap <- integer(length(beg))
-  for(i in seq_along(beg)) 
-    submap[i] <- beg[i] + sample.int( end[i] - beg[i] + 1L , 1L) - 1L
-  
-  return(submap)
-}
