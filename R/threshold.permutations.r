ee5e25ddc613220c9d528239f6b01b506ef9e991
SidonieFoulon <sidonie.foulon@inserm.fr>
2026-03-16 17:13:03 +0100

    fixed threshold.permutation

--- R/threshold.permutations.r
+++ R/threshold.permutations.r
@@ -6,32 +6,9 @@
 
 #' @export threshold.permutations
 
-get.z.max.unit <- function(iteration) {
-  cases <- sample(which(pheno == 0 | pheno == 1), nb.cases, replace = FALSE) #only on non-NA phenotypes
-  controls <- sample(which(pheno == 0 | pheno == 1)[-cases], nb.controls, replace = FALSE) #only on non-NA phenotypes
-  pheno[cases] <- 1
-  pheno[controls] <- 0
-
-  reg <- HBD.glm(x = atlas, expl_var = expl_var, phen = pheno, phen.code = "R", score = score, variance = var, pval = FALSE)
-
-  return(max(reg$z.value))
-  #z.max[j] <- max(reg$z.value)
-}
 
 
-calc.z.max <- function(fin, score){
-  deb <- ifelse(score, 1,)
-  var <- ifelse(score, sigma2, NULL)
-  
-  library(parallel)
-  cl <- makeCluster(Fantasio.parameters(n_threads)) # créer le cluster
-  results.all.z.max <- parLapply(cl, deb:fin, get.z.max.unit) # boucle for inclue dans parLapply
-  on.exit(stopCluster(cl))
-  
-  z.max <- c(z.max, results.all.z.max)  
-}
-
-threshold.permutations <- function(atlas, nb.perm = 1000, expl.var = c("FLOD", "pHBD"), phen, phen.code = c("R", "plink"), score){
+threshold.permutations <- function(atlas, nb.perm = 1000, expl.var = c("FLOD", "pHBD"), phen, phen.code = c("R", "plink"), score, cores){
 
   # phenotype coding
 
@@ -60,6 +37,7 @@ threshold.permutations <- function(atlas, nb.perm = 1000, expl.var = c("FLOD", "
 
   z.max <- list()
   
+  
   if(score) {
     #first get the variance for all permutations
     cases <- sample(which(pheno == 0 | pheno == 1), nb.cases, replace = FALSE) #only on non-NA phenotypes
@@ -72,9 +50,42 @@ threshold.permutations <- function(atlas, nb.perm = 1000, expl.var = c("FLOD", "
     sigma2 <- reg$variance
     
     #then compute for the rest of the permutations    
+  } else {
+    force(atlas)
   }
-  calc.z.max(nb.perm, score)
+  
+  get.z.max.unit <- function(iteration) {
+    cases <- sample(which(pheno == 0 | pheno == 1), nb.cases, replace = FALSE) #only on non-NA phenotypes
+    controls <- sample(which(pheno == 0 | pheno == 1)[-cases], nb.controls, replace = FALSE) #only on non-NA phenotypes
+    pheno[cases] <- 1
+    pheno[controls] <- 0
+    
+    variance <- if (score) sigma2 else NULL
 
+    reg <- HBD.glm(x = atlas, expl_var = expl_var, phen = pheno, phen.code = "R", score = score, variance = variance, pval = FALSE)
+
+    return(max(reg$z.value))
+    #z.max[iteration] <- max(reg$z.value)
+  }
+
+
+  calc.z.max <- function(fin, score){
+    deb <- ifelse(score, 2,1)
+    
+  
+    library(parallel)
+    cl <- makeCluster(cores) # créer le cluster
+    clusterSetRNGStream(cl) # L ecuyer
+    results.all.z.max <- parLapply(cl, deb:fin, get.z.max.unit) # boucle for inclue dans parLapply
+    on.exit(stopCluster(cl))
+  
+    return(results.all.z.max)
+    #z.max <- c(z.max, results.all.z.max)  
+  }
+  
+  all.z.max <- calc.z.max(nb.perm, score)
+
+  z.max <- c(z.max, all.z.max)
   z.max <- unlist(z.max)
   z.max.95 <- quantile(z.max, 0.95)
 
