9947b382b6600c6d90e6564d948e3f1549c13a99
SidonieFoulon <sidonie.foulon@inserm.fr>
2026-05-10 18:17:22 +0200

    computed permutation thresholds for pvalue for bilateral and unilaterals left and right tests

--- R/threshold.permutations.r
+++ R/threshold.permutations.r
@@ -35,7 +35,7 @@ threshold.permutations <- function(atlas, nb.perm = 1000, expl.var = c("FLOD", "
   nb.cases <- sum(pheno == 1, na.rm=TRUE)
   nb.controls <- sum(pheno == 0, na.rm=TRUE)
 
-  z.max <- list()
+  z.max.min <- list()
   
   
   if(score) {
@@ -46,7 +46,7 @@ threshold.permutations <- function(atlas, nb.perm = 1000, expl.var = c("FLOD", "
     pheno[controls] <- 0
     
     reg <- HBD.glm(x = atlas, expl_var = expl_var, phen = pheno, phen.code = "R", score = TRUE, pval = FALSE)
-    z.max[1] <- max(reg$z.value)
+    z.max.min[[1]] <- c(zmax = max(reg$z.value), zmin = min(reg$z.value))
     sigma2 <- reg$variance
     
     #then compute for the rest of the permutations    
@@ -67,7 +67,7 @@ threshold.permutations <- function(atlas, nb.perm = 1000, expl.var = c("FLOD", "
 
     reg <- HBD.glm(x = atlas, expl_var = expl_var, phen = pheno, phen.code = "R", score = score, variance = variance, pval = FALSE)
 
-    return(max(reg$z.value))
+    return(c(zmax = max(reg$z.value), zmin = min(reg$z.value)))
     #z.max[iteration] <- max(reg$z.value)
   }
 
@@ -93,13 +93,21 @@ threshold.permutations <- function(atlas, nb.perm = 1000, expl.var = c("FLOD", "
     #z.max <- c(z.max, results.all.z.max)  
   }
   
-  all.z.max <- calc.z.max(nb.perm, score)
+  all.z.max.min <- calc.z.max(nb.perm, score)
 
-  z.max <- c(z.max, all.z.max)
-  z.max <- unlist(z.max)
+  z.max.min <- c(z.max.min, all.z.max.min)
+  z.max <- sapply(z.max.min, function(x) x['zmax'])
+  z.min <- sapply(z.max.min, function(x) x['zmin'])
   z.max.95 <- quantile(z.max, 0.95)
+  z.min.95 <- quantile(z.min, 0.05)
+  p.left <- -log10(pnorm(z.min.95))
+  p.bil <- -log10(pchisq(max(abs(z.min.95), abs(z.max.95))**2, df = 1, lower.tail = FALSE))
+  p.right <- -log10(1-pnorm(z.max.95))
 
-  res <- list(z.max = z.max, threshold = z.max.95, signif = (z.max > z.max.95))
+  res <- list(z.max = z.max, threshold.z.max = z.max.95,
+              z.min = z.min, threshold.z.min = z.min.95,
+              p.left = p.left, p.bilateral = p.bil, p.right = p.right,
+              signif = (z.max > z.max.95))
 
   res
 
